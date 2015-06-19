169dcac873436f2fe8059acbdaab632bdaccdbc9
Xing Xing <mikespook@gmail.com>
2015-06-19 19:35:04 +0800

    re-implement the signature function

--- handle.go
+++ handle.go
@@ -1,65 +1,51 @@
 package wechat
 
 import (
-    "fmt"
-    "sort"
-    "net/http"
-    "io/ioutil"
-    "crypto/sha1"
-    "github.com/mikespook/golib/log"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/mikespook/golib/log"
 )
 
 var (
-    Token = "thisiswechattoken"
+	Token = "thisiswechattoken"
 )
 
-type HandlerFunc func(*Request)(*Response, error)
+type HandlerFunc func(*Request) (*Response, error)
 
 func Handle(w http.ResponseWriter, r *http.Request, h HandlerFunc) {
-    defer r.Body.Close()
-    if r.Method == "POST" || r.Method == "post" {
-        body, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-            log.Error(err)
-            w.WriteHeader(500)
-            return
-        }
-        var wreq *Request
-        if wreq, err = DecodeRequest(body); err != nil {
-            log.Error(err)
-            w.WriteHeader(500)
-            return
-        }
-        wresp, err := h(wreq)
-        if err != nil {
-            log.Error(err)
-            w.WriteHeader(500)
-            return
-        }
-        data, err := wresp.Encode()
-        if _, err := w.Write(data); err != nil {
-            log.Error(err)
-            w.WriteHeader(500)
-        }
-        return
-    } else {
-        if Signature(Token, r.FormValue("timestamp"),
-            r.FormValue("nonce")) == r.FormValue("signature") {
-            w.Write([]byte(r.FormValue("echostr")))
-        } else {
-            w.WriteHeader(403)
-        }
-    }
-}
-
-func Signature(token, timestamp, nonce string) string {
-    strs := sort.StringSlice{token, timestamp, nonce}
-    sort.Strings(strs)
-    str := ""
-    for _, s := range strs {
-        str += s
-    }
-    h := sha1.New()
-    h.Write([]byte(str))
-    return fmt.Sprintf("%x", h.Sum(nil))
+	defer r.Body.Close()
+	if r.Method == "POST" || r.Method == "post" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Error(err)
+			w.WriteHeader(500)
+			return
+		}
+		var wreq *Request
+		if wreq, err = DecodeRequest(body); err != nil {
+			log.Error(err)
+			w.WriteHeader(500)
+			return
+		}
+		wresp, err := h(wreq)
+		if err != nil {
+			log.Error(err)
+			w.WriteHeader(500)
+			return
+		}
+		data, err := wresp.Encode()
+		if _, err := w.Write(data); err != nil {
+			log.Error(err)
+			w.WriteHeader(500)
+		}
+		return
+	} else {
+		if Signature(Token, r.FormValue("timestamp"),
+			r.FormValue("nonce")) == r.FormValue("signature") {
+			w.Write([]byte(r.FormValue("echostr")))
+		} else {
+			w.WriteHeader(403)
+		}
+	}
 }
